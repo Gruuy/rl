f032be6cbd08fb27e5a926c8b8884f6f2879191c
Gruuy <245746119@qq.com>
2019-03-19 20:55:00 +0800

    增加了Pagehelper帮助分页

--- src/main/Test/Test.java
+++ src/main/Test/Test.java
@@ -1,5 +1,7 @@
 import com.Gruuy.entity.User;
 import com.Gruuy.service.UserService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -19,8 +21,15 @@ public class Test {
     private JedisPool jedisPool;
     @org.junit.Test
     public void Test(){
+        //测试pagehelper
+        PageHelper.startPage(1,1);
         List<User> userList=userService.getAllUser();
-        System.out.println(userList.toString());
+        //pageinfo对象封装了许多属性，贼几把好用就对了
+        PageInfo<User> page=new PageInfo<User>(userList);
+        System.out.println(page.getList().get(0).getId());
+        System.out.println(page.getList().get(0).getUsername());
+        System.out.println(page.getList().get(0).getPassword());
+        System.out.println(page.getList().get(0).getLevel());
     }
     @org.junit.Test
     public void TestRedis(){
